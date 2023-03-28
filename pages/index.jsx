c7c223f71108276d576c32dbbc2fb4fe2bd933ba
Oleksii Melnyk <oleksii0@coderiver.com.ua>
2023-03-28 17:03:38 +0300

    add select

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,33 @@
-import { ButtonOutline, Htag, Wrapper } from '@/components/common';
+import { useState } from 'react';
+
+import { ButtonOutline, Htag, Select, Wrapper } from '@/components/common';
 import { widthLayout } from '@/layout/Layout';
 
 import TestIcon from '@/assets/icons/heart.svg';
 
+const options = [
+  { value: 'chocolate', label: 'Chocolate', icon: <TestIcon /> },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
 function Home() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
-    <>
+    <div style={{ height: '100vh' }}>
       <Wrapper>
         HOME
         <ButtonOutline>ButtonOutline</ButtonOutline>
         <TestIcon />
         <Htag tag="h3">TEST Title</Htag>
+        <Select
+          options={options}
+          selectedOption={selectedOption}
+          onChange={setSelectedOption}
+        />
       </Wrapper>
-    </>
+    </div>
   );
 }
 
