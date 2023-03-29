dec4fc3ebe05b1a41ca1ca96c76bb270316d0f68
Olena Pohybil <itlentus@gmail.com>
2023-03-29 18:11:47 +0300

    add accent lines

--- components/common/Switch/Switch.jsx
+++ components/common/Switch/Switch.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-import s from './Switch.module.scss';
+// import s from './Switch.module.scss';
 
-export const Switch = (props) => {
-  const { isChecked = false, ...chProps } = props;
-  return (
-    <label className={s.Switch} labelfor="checkbox">
-      <input
-        className={s.Checkbox}
-        type="checkbox"
-        id="checkbox"
-        checked={isChecked}
-      />
-      <span className={s.Slider}></span>
-    </label>
-  );
-};
+// export const Switch = (props) => {
+//   const { isChecked = false, ...chProps } = props;
+//   return (
+//     <label className={s.Switch} labelfor="checkbox">
+//       <input
+//         className={s.Checkbox}
+//         type="checkbox"
+//         id="checkbox"
+//         checked={isChecked}
+//       />
+//       <span className={s.Slider}></span>
+//     </label>
+//   );
+// };
 
-Switch.propTypes = {
-  checked: PropTypes.bool
-};
+// Switch.propTypes = {
+//   checked: PropTypes.bool
+// };
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,8 @@ import {
   Logo,
   Label,
   Switch,
-  Input
+  Input,
+  AccentH
 } from '@/components/common';
 import { widthLayout } from '@/layout/Layout';
 import { useState } from 'react';
@@ -33,19 +34,27 @@ function Home() {
         <TestIcon />
         <Chip className="active" name="hello" count={155} />
         <Htag tag="h3">TEST Title</Htag>
+        <Htag>Тут і зараз</Htag>
         <Select
           options={options}
           selectedOption={selectedOption}
           onChange={setSelectedOption}
         />
         <Label name="new label" />
-        <Switch isChecked={false} />
+        {/* <Switch isChecked={false} /> */}
         <Input
           placeholder="Input"
           onChange={(e) => setInputValue(e.target.value)}
           value={inputValue}
           type="textarea"
         />
+        <AccentH tag='h2' location='specialists'>яких ви шукаєте</AccentH>
+        <AccentH tag='h2' location='companies'>50+ компаній,</AccentH>
+        <AccentH tag='h2' location='partners'>1400 компаній</AccentH>
+        <AccentH tag='h2' location='cooperation'>успішної співпраці</AccentH>
+        <AccentH tag='h2' location='contacts'>NEWSTAFF ВІДКРИТИЙ</AccentH>
+        <AccentH tag='h2' location='about'>NEWSTAFF</AccentH>
+        <AccentH location='works'>в двох словах</AccentH>
       </Wrapper>
     </div>
   );
