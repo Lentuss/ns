e2d2b7b5f942b5ef6c43249e333ef70e18885a04
Oleksii Melnyk <oleksii0@coderiver.com.ua>
2023-03-30 10:55:01 +0300

    layout outstaff

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
 import { widthLayout } from '@/layout/Layout';
 import { Outstaff } from '@/components';
+import { Input } from '@/components/common';
 
 function Home() {
   return (
     <div style={{ height: '100vh' }}>
       <Outstaff />
+
+      <Input placeholder="Test" />
     </div>
   );
 }
