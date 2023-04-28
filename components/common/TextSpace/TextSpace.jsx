156cd98711bb9380e6196953886357e4003bd637
Olena Pohybil <itlentus@gmail.com>
2023-04-28 17:18:11 +0300

    without components

--- components/common/TextSpace/TextSpace.jsx
+++
@@ -1,3 +0,0 @@
-export const TextSpace = () => {
-  return <span style={{ lineHeight: 0 }}>&nbsp;</span>;
-};
--- components/modals/index.js
+++
@@ -1,5 +0,0 @@
-export * from './SpecialistsModal/SpecialistsModal';
-export * from './SpecialistsFormModal/SpecialistsFormModal';
-export * from './HomeFormModal/HomeFormModal';
-export * from './SuccessModal/SuccessModal';
-export * from './CookiesModal/CookiesModal';
