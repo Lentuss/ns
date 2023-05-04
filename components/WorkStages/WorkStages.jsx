af21f5e69565bdfbf38e40aa22aba8f551199b4c
Olena Pohybil <itlentus@gmail.com>
2023-05-04 17:06:35 +0300

    wstages anim decrease

--- components/WorkStages/WorkStages.jsx
+++ components/WorkStages/WorkStages.jsx
@@ -24,7 +24,8 @@ export const WorkStages = () => {
                     scrub: true,
                     snap: {
                         snapTo: "labelsDirectional",
-                        duration: { min: 0.1, max: 0.3 },
+                        // duration: { min: 0.1, max: 0.3 },
+                        duration: 0.1,
                         ease: "power1.inOut"
                     }
                 }
@@ -32,56 +33,56 @@ export const WorkStages = () => {
                 .addLabel('stage 1')
                 .to('.box5', {
                     xPercent: 100,
-                    duration: 0.3
+                    duration: 0.1
                 })
                 .to(sections, {
                     yPercent: -100,
                     ease: "none",
-                    duration: 0.3,
-                }, '-=0.3')
+                    duration: 0.1,
+                }, '-=0.1')
                 .to('.box8', {
                     xPercent: 100,
-                    duration: 0.3
-                }, '-=0.3')
+                    duration: 0.1
+                }, '-=0.1')
                 .addLabel('stage 2')
                 .to('.box4', {
-                    yPercent: -100, duration: 0.3
+                    yPercent: -100, duration: 0.1
                 })
                 .to('.box11', {
-                    yPercent: -100, duration: 0.3
-                }, '-=0.3')
+                    yPercent: -100, duration: 0.1
+                }, '-=0.1')
                 .to(sections, {
                     yPercent: -200,
                     ease: "none",
-                    duration: 0.3,
-                }, '-=0.3')
+                    duration: 0.1,
+                }, '-=0.1')
                 .to('.box15', {
-                    yPercent: -100, duration: 0.3
-                }, '-=0.3')
+                    yPercent: -100, duration: 0.1
+                }, '-=0.1')
                 .addLabel('stage 3')
                 .to('.box13', {
-                    yPercent: -300, duration: 0.3
+                    yPercent: -300, duration: 0.1
                 })
                 .to('.box14', {
                     yPercent: -200, xPercent: -100
-                }, '-=0.3')
+                }, '-=0.1')
                 .to('.box5', {
-                    yPercent: 100, duration: 0.3
-                }, '-=0.3')
+                    yPercent: 100, duration: 0.1
+                }, '-=0.1')
                 .to(sections, {
                     yPercent: -300,
                     ease: "none",
-                    duration: 0.3,
-                }, '-=0.3')
+                    duration: 0.1,
+                }, '-=0.1')
                 .to('.box2', {
-                    yPercent: 100, xPercent: 100, duration: 0.3
-                }, '-=0.3')
+                    yPercent: 100, xPercent: 100, duration: 0.1
+                }, '-=0.1')
                 .to('.box8', {
-                    yPercent: 100, duration: 0.3
-                }, '-=0.3')
+                    yPercent: 100, duration: 0.1
+                }, '-=0.1')
                 .to('.box15', {
-                    yPercent: 0, duration: 0.3
-                }, '-=0.3')
+                    yPercent: 0, duration: 0.1
+                }, '-=0.1')
                 .addLabel('stage 4')
 
         }, ref);
